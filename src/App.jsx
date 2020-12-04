b0bbfaa6d776ceea6877245f69f596985d0216be
Jonas Gossens <jonasgo10@gmail.com>
2020-12-04 10:25:20 +0100

    Add time slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import MapboxMap from "./mapboxMap/MapboxMap";
 import React from "react";
+import TimeBox from "./timeBox/TimeBox";
 
 function App() {
     return (
         <div className="App">
             <MapboxMap/>
+            <TimeBox/>
         </div>
     );
 }
